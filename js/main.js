c003ba1f7d5c2a4109291efb60cc8b41a071b821
Aaron <aaronariasramos@gmail.com>
2021-02-04 22:03:03 -0700

    estructura general e inicio codigo js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('Creado');
+// Asignación de elemento contenedor.
+var contenedorLista = document.getElementById('contenedorLista');
+
+// Crear título con etiqueta h1 y su texto.
+var tituloLista = document.createElement('h1');
+var textoTitulo = document.createTextNode('To Do List');
+
+// Agregar estilos
+// ------------------------------------------------
+
+// Contenedor
+contenedorLista.style.backgroundColor = 'black';
+contenedorLista.style.padding = '20px';
+contenedorLista.style.textAlign = 'center';
+
+// Título
+tituloLista.style.color = 'white';
+
+// ------------------------------------------------
+
+// Agregar el texto al h1.
+tituloLista.appendChild(textoTitulo);
+
+// Agregar el título al contenedor.
+contenedorLista.appendChild(tituloLista);
