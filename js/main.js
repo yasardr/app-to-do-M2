c9c56341c0da46b4220be9b0d923824d2719f950
Jovheiry <jovheiry@gmail.com>
2021-02-05 21:37:53 -0600

    Variables Names to Standar

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 // Asignación de elemento contenedor.
-var contenedorLista = document.getElementById('contenedorLista');
+var listContainer = document.getElementById('contenedorLista');
 
 // Crear título con etiqueta h1 y su texto.
-var tituloLista = document.createElement('h1');
-var textoTitulo = document.createTextNode('To Do List');
+var appTitle = document.createElement('h1');
+var titleName = document.createTextNode('To Do List');
+
+var textInputTask = document.create
 
 // Agregar estilos
 // ------------------------------------------------
 
 // Contenedor
-contenedorLista.style.backgroundColor = 'black';
-contenedorLista.style.padding = '20px';
-contenedorLista.style.textAlign = 'center';
+listContainer.style.backgroundColor = 'black';
+listContainer.style.padding = '20px';
+listContainer.style.textAlign = 'center';
 
 // Título
-tituloLista.style.color = 'white';
+appTitle.style.color = 'white';
 
 // ------------------------------------------------
 
 // Agregar el texto al h1.
-tituloLista.appendChild(textoTitulo);
+appTitle.appendChild(titleName);
 
 // Agregar el título al contenedor.
-contenedorLista.appendChild(tituloLista);
+listContainer.appendChild(appTitle);
+
