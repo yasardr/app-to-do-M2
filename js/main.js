1581b3ed170321030c0de32f56aaf4c1afec8dcd
Jovheiry <jovheiry@gmail.com>
2021-02-05 22:45:50 -0600

    Adding Containers and Elements

--- js/main.js
+++ js/main.js
@@ -1,28 +1,58 @@
+
+
 // Asignación de elemento contenedor.
 var listContainer = document.getElementById('contenedorLista');
 
-// Crear título con etiqueta h1 y su texto.
+// Creacion de las Variables de los Elementos
 var appTitle = document.createElement('h1');
-var titleName = document.createTextNode('To Do List');
+var addTaskButton = document.createElement('button');
+var taskContainer = document.createElement('section');
+var taskName = document.createElement('h3');
+var deleteField = document.createElement('p');
+var textInput = document.createElement('input');
+var headerTaskListDiv =  document.createElement('div');
 
-var textInputTask = document.create
+// Adding Propierties to Input
+textInput.type = 'text';
+textInput.name = 'taskNameField';
 
-// Agregar estilos
-// ------------------------------------------------
-
-// Contenedor
+// Adding Container Styles
 listContainer.style.backgroundColor = 'black';
 listContainer.style.padding = '20px';
 listContainer.style.textAlign = 'center';
 
-// Título
-appTitle.style.color = 'white';
+taskContainer.style.backgroundColor = 'white';
+taskContainer.style.padding = '20px';
+taskContainer.style.marginTop = '20px';
+taskContainer.style.textAlign = 'left';
 
-// ------------------------------------------------
+// Adding Text Style
+
+
+// Adding Inner Text and Styles
+
+appTitle.style.color = 'white';
+taskName.color = 'black';
+
+addTaskButton.innerText = 'Add Task';
+deleteField.innerText = 'Delete'
+taskName.innerText = 'Task:';
+
+//
+taskContainer.className = 'container';
+headerTaskListDiv.className = 'row';
+taskName.className = 'col-sm';
+deleteField.className = 'col-sm';
 
 // Agregar el texto al h1.
-appTitle.appendChild(titleName);
+appTitle.innerText = 'To Do List'
 
-// Agregar el título al contenedor.
+// Adding Elements to Structure
+
 listContainer.appendChild(appTitle);
-
+listContainer.appendChild(textInput);
+listContainer.appendChild(addTaskButton);
+listContainer.appendChild(taskContainer);
+taskContainer.appendChild(headerTaskListDiv);
+headerTaskListDiv.appendChild(taskName);
+headerTaskListDiv.appendChild(deleteField);
